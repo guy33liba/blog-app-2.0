c971cd50d020007a6884051c4aa178adf014394c
guy33liba <guy33liba@gmail.com>
2024-11-11 23:43:23 +0200

    server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from "express"
-
+import cors from "cors"
 const app = express()
 
-app.use(express.json())
-app.get("/", (req, res) => {
+app.use(cors())
+app.post("/", (req, res) => {
  res.send("Welcome to the api")
 })
 app.listen(4000)
