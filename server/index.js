1813b808c655d5cd8b72be248befe2973ec8532c
guy33liba <guy33liba@gmail.com>
2024-11-14 09:51:20 +0200

    server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ import cors from "cors"
 const app = express()
 
 app.use(cors())
+app.use(express.json())
 app.post("/", (req, res) => {
- res.send("Welcome to the api")
+ const { username, password } = req.body
+ res.json({ requestData: { username, password } })
 })
 app.listen(4000)
