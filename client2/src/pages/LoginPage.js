d5b234ad9a4b24975bd8da2f3ab8d9ee6f7f8259
Guy Liba <guy33liba@gmail.com>
2024-11-14 13:39:12 +0200

    start

--- client2/src/pages/LoginPage.js
+++ client2/src/pages/LoginPage.js
@@ -1,15 +1,14 @@
 import React from "react"
 
 const LoginPage = () => {
- return (
-  <form>
-   <h1>Login</h1>
-
-   <input type="text" className="" placeholder="username" />
-   <input type="password" placeholder="password" />
-   <button>Login</button>
-  </form>
- )
+  return (
+    <form>
+      <h1>Login</h1>
+      <input type="text" className="" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button>Login</button>
+    </form>
+  )
 }
 
 export default LoginPage
