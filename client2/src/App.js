2943532b28636ef7528cdb014193a197077f8a0e
Guy Liba <guy33liba@gmail.com>
2024-11-17 15:55:43 +0200

    contexts

--- client2/src/App.js
+++ client2/src/App.js
@@ -4,16 +4,19 @@ import Layout from "./Layout"
 import IndexPage from "./pages/IndexPage"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
+import { userContextProvider } from "./Usercontext"
 function App() {
- return (
-  <Routes>
-   <Route path="/" element={<Layout />}>
-    <Route index element={<IndexPage />} />
-    <Route path={"/login"} element={<LoginPage />} />
-    <Route path={"/register"} element={<RegisterPage />} />
-   </Route>
-  </Routes>
- )
+  return (
+    <userContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path={"/login"} element={<LoginPage />} />
+          <Route path={"/register"} element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </userContextProvider>
+  )
 }
 
 export default App
