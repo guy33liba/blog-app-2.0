31200e1ba6cc936275a0dd5bac9f45c528f7c969
guy33liba <guy33liba@gmail.com>
2024-11-18 08:37:28 +0200

    server

--- client2/src/pages/CreatePost.js
+++ client2/src/pages/CreatePost.js
@@ -5,14 +5,40 @@ export default function CreatePost() {
  const [title, settitle] = useState("")
  const [summary, setsummary] = useState("")
  const [content, setcontent] = useState("")
+ const [files, setFiles] = useState("")
  const formats = ["header", "bold", "italic", "underline", "strike"]
+
+ function createNewPost(e) {
+  e.preventDefault()
+  const data = new FormData()
+  data.set("title", title)
+  data.set("summary", summary)
+  data.set("content", content)
+  data.set("file", files[0])
+  console.log(files)
+  fetch("http://localhost:4000/post"),
+   {
+    method: "POST",
+    body: data,
+   }
+ }
  return (
-  <form>
-   <input type="title" placeholder="Title" />
-   <input type="summary" placeholder="Summary" />
-   <input type="file" />
-   <ReactQuill value={content} />
-   <button>Create Post</button>
+  <form onSubmit={createNewPost} className="createPost">
+   <input
+    type="title"
+    placeholder="Title"
+    value={title}
+    onChange={(e) => settitle(e.target.value)}
+   />
+   <input
+    type="summary"
+    placeholder="Summary"
+    value={summary}
+    onChange={(e) => setsummary(e.target.value)}
+   />
+   <input type="file" onChange={(e) => setFiles(e.target.files)} />
+   <ReactQuill value={content} onChange={(e) => setcontent(e)} />
+   <button className="createPostButton">Create Post</button>
   </form>
  )
 }
